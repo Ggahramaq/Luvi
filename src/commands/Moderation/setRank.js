2e66d1cb256c2e485b043c6123759582edcec7ff
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2024-04-29 09:34:24 +0400

    Bugfix

--- src/commands/Moderation/setRank.js
+++ src/commands/Moderation/setRank.js
@@ -22,25 +22,28 @@ module.exports = {
     await interaction.deferReply();
     const guildId = interaction.guildId;
     let user;
-    const userId = interaction.user.id;
-    const member =interaction.options?.get('member');
+    const userId = interaction.user.id; 
+    const member = interaction.options?.get('member');
+    const targetUserId = member.user.id;
     const newLvl = parseFloat(interaction.options?.get('level')?.value);
 
-    user = await User.findOne({ guildId, userId});
+
+    
+    user = await User.findOne({ guildId: guildId, userId: targetUserId });
 
     try{
         if (newLvl === 0){
             await User.findOneAndUpdate(
-                {guildId, userId},{level: newLvl+1, xp:(((newLvl) * (newLvl) * (newLvl)) * 25) },{upsert:true, new:true}
+                {guildId: guildId, userId: targetUserId},{level: newLvl+1, xp:(((newLvl) * (newLvl) * (newLvl)) * 25) },{upsert:true, new:true}
             ).then(() => interaction.editReply({ content: `Успешно поменяли ранг ${member.user.username} на ${newLvl+1}(нельзя поменять уровень на 0)`}))
         } else {
             if (newLvl === 1) {
             await User.findOneAndUpdate(
-                {guildId, userId},{level: newLvl, xp:(((newLvl-1) * (newLvl-1) * (newLvl-1)) * 25) },{upsert:true, new:true}
+                {guildId: guildId, userId: targetUserId},{level: newLvl, xp:(((newLvl-1) * (newLvl-1) * (newLvl-1)) * 25) },{upsert:true, new:true}
             ).then(() => interaction.editReply({ content: `Успешно поменяли ранг ${member.user.username} на ${newLvl}`}))
         } else {
             await User.findOneAndUpdate(
-                {guildId, userId},{level: newLvl, xp:(((newLvl-1) * (newLvl-1) * (newLvl-1)) * 25) + 20},{upsert:true, new:true}
+                {guildId: guildId, userId: targetUserId},{level: newLvl, xp:(((newLvl-1) * (newLvl-1) * (newLvl-1)) * 25) + 20},{upsert:true, new:true}
             ).then(() => interaction.editReply({ content: `Успешно поменяли ранг ${member.user.username} на ${newLvl}`}))  
         }
     }
